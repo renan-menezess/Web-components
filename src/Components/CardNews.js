b6adef0d5b13f000d8ada57cc7b24f38f6162f22
Renan Menezes <2003rcsm@gmail.com>
2024-01-17 17:07:34 -0300

    Criando Style do component

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -19,6 +19,7 @@ class CardNews extends HTMLElement {
 
         const linkTitle = document.createElement("a");
         linkTitle.textContent = this.getAttribute("title");
+        linkTitle.href = this.getAttribute("link-url");
 
         const newsContent = document.createElement("p");
         newsContent.textContent = this.getAttribute("content");
@@ -31,6 +32,8 @@ class CardNews extends HTMLElement {
         cardRight.setAttribute("class", "card__right");
 
         const newsImage = document.createElement("img");
+        newsImage.src = this.getAttribute("photo") || "./assets/default-photo.png";
+        newsImage.alt = "Foto da Noticia";
         cardRight.appendChild(newsImage);
 
         componentRoot.appendChild(cardLeft);
@@ -39,7 +42,50 @@ class CardNews extends HTMLElement {
         return componentRoot;
     }
 
-    styles() {}
+    styles() {
+        const style = document.createElement("style");
+        style.textContent = `
+    .card {
+        width: 80%;
+        display: flex;
+        flex-direction: row;
+        -webkit-box-shadow: 10px 6px 31px 3px rgba(0,0,0,0.75);
+        -moz-box-shadow: 10px 6px 31px 3px rgba(0,0,0,0.75);
+        box-shadow: 10px 6px 31px 3px rgba(0,0,0,0.75);
+        justify-content: space-between;
+    }
+    
+    .card__left {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        padding-left: 10px;
+    }
+    
+    .card__left > span {
+        font-weight: 400;
+    }
+    
+    .card__left > a {
+        margin-top: 15px;
+        font-size: 25px;
+        color: rgb(0, 0, 0);
+        text-decoration: none;
+        font-weight: bold;
+    }
+    
+    .card__left > h1 {
+        margin-top: 10px;
+        font-size: 25px;
+    }
+    
+    .card__left > p {
+        color: rgb(128, 128, 128);
+    }
+    `;
+
+    return style;
+    }
 }
 
 customElements.define("card-news", CardNews);
