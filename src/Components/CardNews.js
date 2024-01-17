a4a055e0756b6a91fcd6b48af69091168dc7a750
Renan Menezes <2003rcsm@gmail.com>
2024-01-17 16:35:48 -0300

    Component de maneira profissional CardNews

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -2,9 +2,13 @@ class CardNews extends HTMLElement {
     constructor() {
         super();
 
-        const shadow = this.attachShadow({mode: "open"});
+        const shadow = this.attachShadow({mode: "open" });
         shadow.innerHTML = "<h1>Hello World</h1>";
     }
+
+    build() {}
+
+    styles() {}
 }
 
-customElements.define('card-news', CardNews);
+customElements.define("card-news", CardNews);
--- src/Components/TituloDinamico.js
+++ src/Components/TituloDinamico.js
@@ -1,18 +1,18 @@
 class TituloDinamico extends HTMLElement {
-    constructor () {
+    constructor() {
         super();
 
-        const shadow = this.attachShadow({mode: "open"});
+        const shadow = this.attachShadow({ mode: "open" });
 
         //base do component
         const componentRoot = document.createElement("h1");
-        componentRoot.textContent = "Renan";
+        componentRoot.textContent = this.getAttribute("titulo");
 
         //estilizar o component
         const style = document.createElement("style");
         style.textContent = `
-        h1 {
-            color:red;
+        h1{
+            color: red;
         }
         `;
 
