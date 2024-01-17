4706b0d1fc3eddfaf5118ba323239b2ad9e1c834
Renan Menezes <2003rcsm@gmail.com>
2024-01-17 16:41:01 -0300

    Setando propriedades de classes

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -3,10 +3,25 @@ class CardNews extends HTMLElement {
         super();
 
         const shadow = this.attachShadow({mode: "open" });
-        shadow.innerHTML = "<h1>Hello World</h1>";
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.styles());
     }
 
-    build() {}
+    build() {
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card");
+
+        const cardLeft = document.createElement("div");
+        cardLeft.setAttribute("class", "card__left");
+
+        const cardRight = document.createElement("div");
+        cardRight.setAttribute("class", "card__right");
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+
+        return componentRoot;
+    }
 
     styles() {}
 }
